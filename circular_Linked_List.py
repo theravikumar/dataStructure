54f932540fc18087b72018a42c1aae77b2907d76
theravikumar <ravi940107@gmail.com>
2021-04-05 17:56:09 +0530

    functionAdded

--- circular_Linked_List.py
+++ circular_Linked_List.py
@@ -49,12 +49,71 @@ class Circular_singly_LinkedList:
                     newNode.next = nodeCount1.next
                     nodeCount1.next = newNode
 
+    def delete_SLL_Node(self,position):
+        if position == 0:
+            delNode = self.head
+            while delNode.next != self.head:
+                delNode = delNode.next
+            self.head = self.head.next
+            delNode.next = self.head
+        else:
+            count = 0
+            tempNode = self.head
+
+            while tempNode.next != self.head:
+                count = count+1
+                temp = tempNode
+                tempNode = tempNode.next
+            if count == position-1:
+                temp.next.next = None
+                temp.next = self.head
+            else:
+                i = 0
+                delNode = self.head
+                
+                while i<position:
+                    i = i+1
+                    preNode = delNode
+                    delNode = delNode.next
+                preNode.next = delNode.next
+                delNode.next = None
+
+    def search_CSLL_value(self,value):
+        testNode = self.head
+        position = 0
+        while testNode:
+            if testNode.value == value:
+                return(position)
+            position = position + 1
+            testNode = testNode.next
+        return ("value is not stored")
+    def update_CSLL_Node(self,position,value):
+        if position == 0:
+            self.head.value = value
+        else:
+
+            i = 0
+            updateNode = self.head
+            
+            while i<position:
+                i = i+1
+                
+                updateNode = updateNode.next
+            updateNode.value = value
+
+                
+
            
 CircularsinglyLinkedList = Circular_singly_LinkedList()
 CircularsinglyLinkedList.insert_CSLL_Node(0,1)
 CircularsinglyLinkedList.insert_CSLL_Node(1,2)
 CircularsinglyLinkedList.insert_CSLL_Node(2,3)
 CircularsinglyLinkedList.insert_CSLL_Node(3,5)
-CircularsinglyLinkedList.insert_CSLL_Node(0,6)
+CircularsinglyLinkedList.insert_CSLL_Node(3,6)
 # CircularsinglyLinkedList.addNode(2)
+print ([Node.value for Node in CircularsinglyLinkedList])
+CircularsinglyLinkedList.delete_SLL_Node(4)
+print ([Node.value for Node in CircularsinglyLinkedList])
+print(CircularsinglyLinkedList.search_CSLL_value(6))
+CircularsinglyLinkedList.update_CSLL_Node(3,9)
 print ([Node.value for Node in CircularsinglyLinkedList])
